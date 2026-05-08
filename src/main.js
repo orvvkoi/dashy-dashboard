29f1e0afb5a4196ff587e4ed73f140a29e8ab5f2
Alicia Sykes <alicia@omg.lol>
2026-05-08 20:46:58 +0100

    🛂 Fixes OIDC/KC auth guard, cb skip login page (#2117)

--- src/main.js
+++ src/main.js
@@ -62,7 +62,7 @@ const mount = () => app.mount('#app');
 const handleAuthFailure = (provider, err) => {
   ErrorHandler(`Failed to authenticate with ${provider}`, err);
   store.commit(Keys.CRITICAL_ERROR_MSG, `Authentication failed (${provider}). See console for details.`);
-  mount();
+  router.replace({ name: 'login' }).catch(() => {}).finally(mount);
 };
 
 router.isReady().then(() => {
